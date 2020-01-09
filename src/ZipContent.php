981a48f9dead96a64b15e2f1c9f961228fff1505
Orkhan Ahmadov <ahmadov90@gmail.com>
2020-01-09 23:40:24 +0100

    WIP

--- src/ZipContent.php
+++ src/ZipContent.php
@@ -3,13 +3,15 @@
 namespace Orkhanahmadov\LaravelZipValidator;
 
 use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use ZipArchive;
 
 class ZipContent implements Rule
 {
     /**
-     * @var array
+     * @var Collection
      */
     private $files;
     /**
@@ -29,21 +31,39 @@ class ZipContent implements Rule
      */
     public function __construct($files, ?string $storage = null)
     {
-        $this->files = is_array($files) ? $files : explode(',', $files);
-        $this->storage = Storage::disk($storage);
+        $this->files = is_array($files) ? collect($files) : collect(explode(',', $files));
+        $this->storage = Storage::disk($storage ?: config('filesystems.default'));
         $this->archive = new ZipArchive();
     }
 
     /**
      * Determine if the validation rule passes.
      *
-     * @param  string  $attribute
-     * @param  mixed  $value
+     * @param string $attribute
+     * @param mixed $value
      * @return bool
      */
     public function passes($attribute, $value): bool
     {
-        //
+        $zipFile = $this->storage->putFileAs($directory = uniqid('zip-'), $value, 'file.zip');
+
+        throw_unless(
+            $this->archive->open($this->storage->path($zipFile)) === true,
+            new FileException('Could not open file.')
+        );
+
+        $this->archive->extractTo($this->storage->path($directory));
+        $this->archive->close();
+
+        $this->storage->delete($zipFile);
+
+        $failedFiles = $this->files->reject(function ($file) use ($directory) {
+            return $this->storage->exists($directory . '/' . $file);
+        });
+
+        $this->storage->deleteDirectory($directory);
+
+        return ! $failedFiles->count();
     }
 
     /**
--- tests/ZipContentTest.php
+++ tests/ZipContentTest.php
@@ -2,26 +2,44 @@
 
 namespace Orkhanahmadov\LaravelZipValidator\Tests;
 
+use Illuminate\Http\UploadedFile;
 use Orkhanahmadov\LaravelZipValidator\ZipContent;
 
 class ZipContentTest extends TestCase
 {
     public function test_returns_true_when_required_string_list_of_files_exist()
     {
-        $validZip = file_get_contents(__DIR__ . '__fixtures__/valid.zip');
+        $this->assertTrue(
+            (new ZipContent('dummy.pdf,image.png,folder_1/text_file.txt'))
+                ->passes(
+                    'attribute',
+                    new UploadedFile(__DIR__ . '/__fixtures__/valid.zip', 'file.zip')
+                )
+        );
 
         $this->assertTrue(
-            (new ZipContent())->passes('attribute', $validZip)
+            (new ZipContent([
+                'dummy.pdf',
+                'image.png',
+                'folder_1/text_file.txt',
+            ]))->passes(
+                'attribute',
+                new UploadedFile(__DIR__ . '/__fixtures__/valid.zip', 'file.zip')
+            )
         );
-    }
-
-    public function test_returns_true_when_required_array_list_of_files_exist()
-    {
-        $this->markTestIncomplete();
     }
 
     public function test_returns_false_when_required_list_of_files_do_not_exist()
     {
-        $this->markTestIncomplete();
+        $this->assertFalse(
+            (new ZipContent([
+                'dummy.pdf',
+                'image.png',
+                'folder_1/text_file.txt',
+            ]))->passes(
+                'attribute',
+                new UploadedFile(__DIR__ . '/__fixtures__/invalid.zip', 'file.zip')
+            )
+        );
     }
 }
